edf21eecbba0a563b12260979b9cf76c4a730d99
Andy Davidoff <andy+github@disruptek.com>
2014-02-16 22:05:59 -0500

    support version and site/globalid specification fix headers as per ebay, for trading/shopping/finding fix spelling in some error messages

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ var buildRequestUrl = function buildRequestUrl(serviceName, params, filters, san
     case 'FindingService':
       if (sandbox) {
         // url =   // @todo
-        throw new Error("Sandbox endpoing for FindingService not yet implemented. Please add.");
+        throw new Error("Sandbox endpoint for FindingService not yet implemented. Please add.");
       }
       else url = "https://svcs.ebay.com/services/search/" + serviceName + "/v1?";
       break;
@@ -93,7 +93,7 @@ var buildRequestUrl = function buildRequestUrl(serviceName, params, filters, san
     case 'Shopping':
       if (sandbox) {
         // url =   // @todo
-        throw new Error("Sandbox endpoing for Shopping service not yet implemented. Please add.");
+        throw new Error("Sandbox endpoint for Shopping service not yet implemented. Please add.");
       }
       else url = "http://open.api.ebay.com/shopping?";
       break;
@@ -110,7 +110,7 @@ var buildRequestUrl = function buildRequestUrl(serviceName, params, filters, san
     default:
       if (sandbox) {
         // url =   // @todo
-        throw new Error("Sandbox endpoing for " + serviceName + " service not yet implemented. Please add.");
+        throw new Error("Sandbox endpoint for " + serviceName + " service not yet implemented. Please add.");
       }
       else url = "https://svcs.ebay.com/" + serviceName + '?';
   }
@@ -178,60 +178,45 @@ var buildXmlInput = function buildXmlInput(opType, params) {
 // for GET requests these go into URL. for POST requests these go into headers.
 // options differ by service, see below.
 var defaultParams = function defaultParams(options) {
-  var params = {},
-  defaultGetParams = {
-    'OPERATION-NAME': options.opType,
-    'GLOBAL-ID': 'EBAY-US',
-    'RESPONSE-DATA-FORMAT': 'JSON',
-    'REST-PAYLOAD': null     // (not sure what this does)
-  };
   options = options || {};
   
-  switch (options.serviceName) {
-    
-    // [GET params >
-    
-    case 'FindingService':
-      params = _.extend({}, defaultGetParams, {
-        'SECURITY-APPNAME': options.appId ? options.appId : null,
-        'SERVICE-VERSION': '1.11.0'
-      });
-      break;
-      
-    case 'MerchandisingService':
-      params = _.extend({}, defaultGetParams, {
-        'SERVICE-NAME': options.serviceName,
-        'CONSUMER-ID': options.appId ? options.appId : null,
-        'SERVICE-VERSION': '1.5.0'   // based on response data
-      });
-      break;
-      
-    case 'Shopping':
-      params = _.extend({}, defaultGetParams, {
-        'appid': options.appId ? options.appId : null,
-        'version': '771',
-        'siteid': '0',
-        'responseencoding': 'JSON',
-        'callname': options.opType
-      });
-      break;
-    
-    
-    // [POST params >
-    
-    case 'Trading':
-      params = {
-        'X-EBAY-API-CALL-NAME' : options.opType,
-        'X-EBAY-API-COMPATIBILITY-LEVEL' : '775',
-        'X-EBAY-API-SITEID' : '0', // US
-        'X-EBAY-API-DEV-NAME': options.devName,
-        'X-EBAY-API-CERT-NAME': options.cert,
-        'X-EBAY-API-APP-NAME': options.appName
-      };
-      break;
-  }
-  
-  return params;
+  return {
+    'FindingService': {
+      'X-EBAY-SOA-SECURITY-APPNAME': options.appId ? options.appId : null,
+      'X-EBAY-SOA-REQUEST-DATA-FORMAT': 'JSON',
+      'X-EBAY-SOA-RESPONSE-DATA-FORMAT': 'JSON',
+      'X-EBAY-SOA-GLOBAL-ID': options.globalId ? options.globalId : 'EBAY-US',
+      'X-EBAY-SOA-SERVICE-VERSION': options.version ? options.version : '1.11.0',
+      'X-EBAY-SOA-OPERATION-NAME': options.opType
+    },
+    'MerchandisingService': {
+      'SERVICE-NAME': options.serviceName,
+      'CONSUMER-ID': options.appId ? options.appId : null,
+      // based on response data
+      'SERVICE-VERSION': options.version ? options.version : '1.5.0',
+      'OPERATION-NAME': options.opType,
+      'GLOBAL-ID': options.globalId ? options.globalId : 'EBAY-US',
+      'RESPONSE-DATA-FORMAT': 'JSON',
+      'REST-PAYLOAD': null     // (not sure what this does)
+    },
+    'Shopping': {
+      'X-EBAY-API-APP-ID': options.appId ? options.appId : null,
+      'X-EBAY-API-VERSION': options.version ? options.version : '771',
+      'X-EBAY-API-SITE-ID': options.siteId ? options.siteId : '0', // US
+      'X-EBAY-API-REQUEST-ENCODING': 'JSON',
+      'X-EBAY-API-RESPONSE-ENCODING': 'JSON',
+      'X-EBAY-API-VERSIONHANDLING': 'LatestEnumValues',
+      'X-EBAY-API-CALL-NAME': options.opType
+    },
+    'Trading': {
+      'X-EBAY-API-COMPATIBILITY-LEVEL' : options.version ? options.version : '775',
+      'X-EBAY-API-SITEID' : options.siteId ? options.siteId : '0', // US
+      'X-EBAY-API-DEV-NAME': options.devName,
+      'X-EBAY-API-CERT-NAME': options.cert,
+      'X-EBAY-API-APP-NAME': options.appName,
+      'X-EBAY-API-CALL-NAME': options.opType
+    }
+  }[options.serviceName];
 };
 
 
