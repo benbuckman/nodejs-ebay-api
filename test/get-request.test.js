7058b4ebf1092db1613d215ab72a21672c9881d7
Ben Buckman <ben@benbuckman.net>
2015-10-03 23:24:03 -0700

    need tests... add some docs, etc.

---
+++ test/get-request.test.js
@@ -0,0 +1,17 @@
+var
+  chai = require('chai'),
+  expect = chai.expect,
+  sinon = require('sinon'),
+  mockery = require('mockery');
+
+chai.use(require("sinon-chai"));
+
+describe('GET request', function() {
+
+  // TODO unit test a Shopping service request ...
+  // stub out `require.get`, make sure it's getting passed the expected options!
+
+  // then try to hit an actual sandbox endpoint ...?
+  // (can a sandbox key be hard-coded in this module?)
+
+});
