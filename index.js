60524d90afb52537809fc2352ee04412c902f55e
Ben Buckman <ben@benbuckman.net>
2015-10-03 23:22:26 -0700

    don't need double function name anymore.

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 
 
 exports.postXmlRequest = require('./lib/post-xml-request').postXmlRequest;
+
 exports.getRequest = require('./lib/get-request').getRequest;
-exports.ItemFilter = require('./lib/filters').ItemFilter;
+
 exports.paginateGetRequest = require('./lib/pagination').paginateGetRequest;
+
+exports.ItemFilter = require('./lib/filters').ItemFilter;
 
