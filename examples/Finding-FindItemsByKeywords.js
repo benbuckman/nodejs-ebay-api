cd0729ce22cc2801783e46278c4952dde001a475
Toby Caulk <tobycaulk@live.com>
2017-12-06 20:56:52 -0500

    Removed AppID from Example

---
+++ examples/Finding-FindItemsByKeywords.js
@@ -0,0 +1,47 @@
+/**
+ * example eBay API request to FindingService:findItemsByKeywords
+ */
+
+var ebay = require('../index.js');
+
+var params = {
+  keywords: ["Canon", "Powershot"],
+
+  // add additional fields
+  outputSelector: ['AspectHistogram'],
+
+  paginationInput: {
+    entriesPerPage: 10
+  },
+
+  itemFilter: [
+    {name: 'FreeShippingOnly', value: true},
+    {name: 'MaxPrice', value: '150'}
+  ],
+
+  domainFilter: [
+    {name: 'domainName', value: 'Digital_Cameras'}
+  ]
+};
+
+ebay.xmlRequest({
+    serviceName: 'Finding',
+    opType: 'findItemsByKeywords',
+    appId: '..........',      // FILL IN YOUR OWN APP KEY, GET ONE HERE: https://publisher.ebaypartnernetwork.com/PublisherToolsAPI
+    params: params,
+    parser: ebay.parseResponseJson,    // (default),
+    country: 'US'
+  },
+  // gets all the items together in a merged array
+  function itemsCallback(error, itemsResponse) {
+    if (error) throw error;
+
+    var items = itemsResponse.searchResult.item;
+
+    console.log('Found', items.length, 'items');
+    
+    for (var i = 0; i < items.length; i++) {
+      console.log('- ' + items[i].title);
+    }  
+  }
+);
