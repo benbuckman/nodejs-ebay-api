46790410299484a0aa9fb039b948fe5f13928dba
Toby Caulk <tobycaulk@live.com>
2017-12-06 20:53:40 -0500

    Access country-specific eBay APIs

--- examples/Finding-FindItemsByKeywords.js
+++ examples/Finding-FindItemsByKeywords.js
@@ -27,9 +27,10 @@ var params = {
 ebay.xmlRequest({
     serviceName: 'Finding',
     opType: 'findItemsByKeywords',
-    appId: '......................',      // FILL IN YOUR OWN APP KEY, GET ONE HERE: https://publisher.ebaypartnernetwork.com/PublisherToolsAPI
+    appId: 'TobyCaul-Flippr-PRD-05d7504c4-b8181fc8',      // FILL IN YOUR OWN APP KEY, GET ONE HERE: https://publisher.ebaypartnernetwork.com/PublisherToolsAPI
     params: params,
-    parser: ebay.parseResponseJson    // (default)
+    parser: ebay.parseResponseJson,    // (default),
+    country: 'US'
   },
   // gets all the items together in a merged array
   function itemsCallback(error, itemsResponse) {
